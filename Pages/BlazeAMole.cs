c4712748b805f317f3f84a0dba150326e3c6d69c
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2024-02-16 17:36:12 +0000

    Setting up the project and adding basics tags

---
+++ Pages/BlazeAMole.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp1game.Pages
+{
+	public partial class BlazeAMole
+	{
+		private int score = 0;
+		private int currentTime = 5;
+		int hitPosition = 0;
+		private string message = "";
+		bool gameIsRunning = true;
+	}
+}
